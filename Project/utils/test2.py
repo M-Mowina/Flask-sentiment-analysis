e4cc1054d110e092fcdcfc9dc3226e0222dc7038
M-Mowina <mhmd.mowena010@gmail.com>
2024-05-11 13:32:11 +0300

    update

--- Project/utils/test2.py
+++ Project/utils/test2.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+import pandas as pd
+
 import tensorflow as tf
 from PIL import Image
 import numpy as np
@@ -18,67 +19,24 @@ lemmatizer = WordNetLemmatizer()
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = tf.keras.models.load_model('amazon_full_model2.h5')
 
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def analysis():
-    return render_template("analysis.html")
-
-
-@app.route('/text', methods=['GET', 'POST'])
-def home():
-    if request.method == 'GET':
-        return render_template('text.html')
-    
-    else:
-        text = request.form['text']
-        print(text)
-        result = predict_sentiment(text)
-        print(result)
-        return render_template('text.html', result=result)
-    
-    
-@app.route('/csv', methods=['GET', 'POST'])
-def csv_file():
-  if request.method == 'POST':
-    # Get uploaded file
-    uploaded_file = request.files['csvfile']
-    if uploaded_file.filename != '':
-      # Read CSV data into a Pandas DataFrame
-      df = pd.read_csv(uploaded_file)
+def predict_csv(csv_file):
+      df = pd.read_csv(csv_file)
       
       # Check if there's a text column for prediction
       if 'text' not in df.columns:
-          return render_template('csv.html', error="CSV must contain a 'text' for prediction")
+          return "CSV must contain a 'text' for prediction"
       
       # Get text data for prediction
-      df['prediction'] = df['text'].apply(lambda text: predict_sentiment(text))
+      text_data = df['text'].tolist()
       
       # Make predictions using your model
-      predictions = df['prediction']
+      predictions = model.predict(text_data)
+      
+      # Add predictions as a new column in the DataFrame
+      df['prediction'] = df['text'].apply(lambda text: predict_sentiment(text))
       
       # Return rendered template with results
-      return render_template('csv.html', data=df.to_html())
-    else:
-      return render_template('csv.html', error="No file selected")
-  else:
-    # Render upload page for GET requests
-    return render_template('csv.html')
-
-
-
-
-
-
-
-
-
-
-
-
-
+      return predictions
 
 
 
@@ -130,20 +88,3 @@ def preprocess_text(text):
   text = ' '.join([word for word in text.split() if word not in stop_words])
 
   return text
-
-def predict_csv(csv_file):
-      df = pd.read_csv(csv_file)
-      
-      # Check if there's a text column for prediction
-      if 'text' not in df.columns:
-          return "CSV must contain a 'text' for prediction"
-      
-      # Add predictions as a new column in the DataFrame
-      df['prediction'] = df['text'].apply(lambda text: predict_sentiment(text))
-      
-      # Return rendered template with results
-      return df['prediction']
-
-
-if __name__ == '__main__':
-    app.run()
